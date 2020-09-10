2c6426b9364ba6fee037fe9d7a41a54fa36e3acc
Opilane TTHK <opilane@tthk.ee>
2020-09-10 12:16:13 +0300

    first commit

---
+++ Task5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal1;
+
+            Console.WriteLine("Personality Test. What kind of animals you like? Dogs Or Cats or something else. ");
+            animal1 = Console.ReadLine();
+            if (animal1 == "cats")
+                Console.WriteLine("You Like cats, you are homebird.");
+            else if (animal1 == "dogs")
+                Console.WriteLine("You like dogs, you are partygoer.");
+            else Console.WriteLine($"You like {animal1}, you are animal friend.");
+
+
+
+            
+
+
+        }    
+    }    
+}
---
+++ Week2IfStatement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace Week2IfStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N1;
+            int N2;
+           
+
+            Console.WriteLine("Write first number.");
+            string Num1 = Console.ReadLine();
+            N1 = Int32.Parse(Num1);
+
+            if (N1 < 0)
+                Console.WriteLine("This number is negative.");
+            else if (N1 > 0)
+                Console.WriteLine("This number is positive.");
+            else Console.WriteLine("This number is 0.");
+                   
+            
+
+
+
+                
+        }
+    }
+}
